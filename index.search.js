5155b9d0c61c57893bb69799eb4337a1ea8a1e00
DIO <wolvie1986@gmail.com>
2023-02-24 11:43:01 +0100

    Aggiorno Notebook 4.1.3

--- index.search.js
+++ index.search.js
@@ -602,7 +602,7 @@ var relearn_search_index = [
     "uri": "/2-appunti/2.3-machine-learning/2.3.6-valutazione-e-ottimizzazione-dei-modelli-di-machine-learning/index.html"
   },
   {
-    "content": "\rI vari progetti sono divisi per aree tematiche:\n4.1-Supervised-Learning Predizione Costo Immobili in New Taipei Rinconoscimento di immagini a bassa risoluzione 4.2-Unsupervised-Learning 4.2.1-Classificazione dei clienti di una Banca 4.3-NLP 4.3.1-Analisi-del-Sentimento 4.4-Reinforced-Learning 4.4.1-Regressione-non-lineare Ogni sezione contiene\nNotebook di lavoro: È presente il codice e l’output relativa all’analisi. Presentazione: in foma di slides e più discorsiva, per capire e risolvere il problema. seconda colonna da riempire ",
+    "content": "\rI vari progetti sono divisi per aree tematiche:\n4.1-Supervised-Learning Predizione Costo Immobili in New Taipei Rinconoscimento di immagini a bassa risoluzione Predizione-Correntisti-Morosi 4.2-Unsupervised-Learning 4.2.1-Classificazione dei clienti di una Banca 4.3-NLP 4.3.1-Analisi-del-Sentimento 4.4-Reinforced-Learning 4.4.1-Regressione-non-lineare Ogni sezione contiene\nNotebook di lavoro: È presente il codice e l’output relativa all’analisi. Presentazione: in foma di slides e più discorsiva, per capire e risolvere il problema. seconda colonna da riempire ",
     "description": "",
     "tags": null,
     "title": "4  -  Progetti",
@@ -658,7 +658,7 @@ var relearn_search_index = [
     "uri": "/index.html"
   },
   {
-    "content": "4.1.1-Regressione-non-lineare 4.1.2-Classificatore-CNN 4.1.3-Classificatore-CNN ",
+    "content": "4.1.1-Regressione-non-lineare 4.1.2-Classificatore-CNN 4.1.3-Predizione-Correntisti-Morosi ",
     "description": "",
     "tags": null,
     "title": "4.1 Supervised learning",
@@ -714,7 +714,7 @@ var relearn_search_index = [
     "uri": "/4-progetti/4.1-supervised-learning/4.1.3-predizione-correntisti-morosi/notebook-di-lavoro/index.html"
   },
   {
-    "content": "",
+    "content": " Notebook di Lavoro ShowCase ",
     "description": "",
     "tags": null,
     "title": "4.1.3 Predizione Correntisti Morosi",
